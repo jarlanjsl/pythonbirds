0aa84ee8001f9478454bb635a4ae04689359e571
Jarlan <silvajarlan@gmail.com>
2021-06-09 19:57:22 -0300

    Criado atibuto complexo filhos

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,17 +1,19 @@
 class Pessoa:
-    def __init__(self, nome=None, idade=29):
+    def __init__(self, *filhos, nome=None, idade=29):
         self.idade = idade
         self.nome = nome
+        self.filhos  = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa('Vitoria')
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
-    print(p.nome)
-    p.nome = 'Jarlan'
-    print(p.nome)
-    print(p.idade)
+    vinicios = Pessoa(nome='Vinicios')
+    vitoria = Pessoa(vinicios, nome='Vitoria')
+    print(Pessoa.cumprimentar(vitoria))
+    print(id(vitoria))
+    print(vitoria.cumprimentar())
+    print(vitoria.nome)
+    print(vitoria.idade)
+    for filho in vitoria.filhos:
+        print(filho.nome)
