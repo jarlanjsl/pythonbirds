9f900ee5d72cd013d73f1fc77e169b37233944e4
Jarlan <silvajarlan@gmail.com>
2021-06-09 20:29:04 -0300

    Criado atributo de classe

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,6 @@
 class Pessoa:
+    olhos = 2
+
     def __init__(self, *filhos, nome=None, idade=29):
         self.idade = idade
         self.nome = nome
@@ -19,6 +21,13 @@ if __name__ == '__main__':
         print(filho.nome)
     vitoria.sobrenome = 'Alves'
     del vitoria.filhos
+    vitoria.olhos = 1
+    del vitoria.olhos
     print(vitoria.sobrenome)
     print(vitoria.__dict__)
     print(vinicios.__dict__)
+    Pessoa.olhos = 3
+    print(Pessoa.olhos)
+    print(vitoria.olhos)
+    print(vinicios.olhos)
+    print(id(Pessoa.olhos), id(vitoria.olhos), id(vinicios.olhos))
